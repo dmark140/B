ac5c827cdb57e1ccf9cea0d6cd4b345dedaa939f
dmark140 <famolerasd@gmail.com>
2023-09-08 15:58:43 +0800

    09082023C

--- src/Components/Head.js
+++ src/Components/Head.js
@@ -7,6 +7,7 @@ export default function Head() {
           <img
             src="https://avatars.githubusercontent.com/u/14121234?s=400&u=c4ff30b44d17298cd8a0fded6258b505007fa891&"
             className="profile"
+            alt=""
           />
         </div>
         <div>
@@ -14,7 +15,9 @@ export default function Head() {
           <p className="text-PreMute p-0 -m-0  text-sm">
             Innovation focused Systems Admin
           </p>
-          <a className="text-Mute p-0 -m-0  text-sm">@CodeWithDean</a>
+          <a href="#" className="text-Mute p-0 -m-0  text-sm">
+            @CodeWithDean
+          </a>
         </div>
       </div>
       {/* <div className="flex gap-x-2">
