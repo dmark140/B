3f57723d528629cc96d56729179ca02285d94bfa
dmark140 <famolerasd@gmail.com>
2024-03-11 21:39:28 +0800

    A

--- src/Components/V3/MainPage/Skills.js
+++ src/Components/V3/MainPage/Skills.js
@@ -1,74 +1,120 @@
 import React from "react";
-import { IconNPM,IconGit, ReactIcon, SAPicon, IconTailwind, SAPSQL, SAPExpress } from "../../Components";
+import {
+  IconNPM,
+  IconGit,
+  ReactIcon,
+  SAPicon,
+  IconTailwind,
+  SAPSQL,
+  SAPExpress,
+  ReactColorIcon,
+  NodeJsIcon,
+  ExpressIcon,
+  TailwindIcon,
+  msSqlIcon,
+  SAPIcon,
+  PostManIcon,
+  gitHubIcon,
+  mysqlIcon,
+  PhpIcon,
+  PsIcon,
+  UnityIcon,
+  BlenderIcon,
+} from "../../Components";
 
 export default function Skills() {
   const skills = [
     {
       label: "ReactJs",
-      icon: ReactIcon,
+      icon: ReactColorIcon,
     },
     {
       label: "NodeJs",
-      icon: ReactIcon,
+      icon: NodeJsIcon,
     },
     {
       label: "Express",
-      icon: ReactIcon,
+      icon: ExpressIcon,
     },
     {
       label: "TailWind",
-      icon: IconTailwind,
-    },    {
+      icon: TailwindIcon,
+    },
+    {
       label: "MSSQL",
-      icon: SAPSQL,
+      icon: msSqlIcon,
     },
     {
       label: "SAP B1",
-      icon: SAPicon,
+      icon: SAPIcon,
     },
     {
       label: "SAP B1 Service Layer",
-      icon: SAPicon,
+      icon: SAPIcon,
     },
     {
       label: "SAP B1 SDK",
-      icon: SAPicon,
+      icon: SAPIcon,
     },
     {
       label: "Crystal Report",
-      icon: SAPicon,
+      icon: SAPIcon,
     },
-  
+    {
+      label: "PostMan",
+      icon: PostManIcon,
+    },
+    {
+      label: "Git Hub",
+      icon: gitHubIcon,
+    },
+    {
+      label: "MySQL",
+      icon: mysqlIcon,
+    },
+    {
+      label: "Php",
+      icon: PhpIcon,
+    },
+    {
+      label: "PhotoShoop",
+      icon: PsIcon,
+    },
+    {
+      label: "Unity3d",
+      icon: UnityIcon,
+    },
+    {
+      label: "Belnder",
+      icon: BlenderIcon,
+    },
   ];
 
   const Tools = [
-    {label: "ERP"},
-    {label: "Project Management"},
-    {label: "System Management"},
-
+    { label: "ERP" },
+    { label: "Project Management" },
+    { label: "System Management" },
   ];
   return (
-   <>
-     <div>
-      <h1 className="font-semibold text-2xl pb-4 mt-10 TITLE ">Tech Stack</h1>
-      <div className="flex flex-wrap gap-2">
-        {skills.map((e, i) => (
-          <div
-            key={i}
-            className="   XALAC  flex border-[2px]  dark:border-black bg-white    text-sm   px-2  rounded-md shadow-sm font-semibold "
-          >
-            <div className="  dark:invert-0 mr-2 ">
-              <e.icon size="24px" className=""/>
-            </div>
-              <div className="text-black ">
-                 {e.label}
+    <>
+      <div>
+        <h1 className="font-semibold text-2xl pb-4 mt-10 TITLE ">Tech Stack</h1>
+        <div className="flex flex-wrap gap-2">
+          {skills.map((e, i) => (
+            <div
+              key={i}
+              className="   XALAC  flex border-[2px]  dark:border-black bg-white    text-sm   px-2  rounded-md shadow-sm font-semibold "
+            >
+              <div className="  mr-2 ">
+                <e.icon size="24px" className="" />
               </div>
-          </div>
-        ))}
+              <div className="text-black ">{e.label}</div>
+            </div>
+          ))}
+        </div>
+        {/* <APIpng/> */}
       </div>
-      {/* <APIpng/> */}
-    </div>
-    {/* <div>
+      {/* <div>
       <h1 className="font-semibold text-2xl pb-1 mt-10 TITLE">Experties</h1>
       <div className="flex flex-wrap gap-2">
         {Tools.map((e, i) => (
@@ -81,8 +127,6 @@ export default function Skills() {
         ))}
       </div>
     </div> */}
-   </>
-
-
+    </>
   );
 }
